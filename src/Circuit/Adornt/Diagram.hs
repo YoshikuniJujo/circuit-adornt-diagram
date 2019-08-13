ec1f1c9b7eb88b9ce4253f91d21ba5af2c6937b0
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-08-13 12:02:39 +0900

    try and gate

--- src/Circuit/Adornt/Diagram.hs
+++ src/Circuit/Adornt/Diagram.hs
@@ -3,6 +3,7 @@
 
 module Circuit.Adornt.Diagram where
 
+import Control.Arrow
 import Control.Monad.State
 import Data.Map.Strict
 
@@ -15,30 +16,51 @@ newtype BG = BG BasicGate deriving Show
 
 instance ElementIdable BG where
 	elementIdGen (BG (NotGate iw)) = "NotGate-" <> BSC.pack (show iw)
+	elementIdGen (BG (AndGate iw1 iw2)) =
+		"AndGate-" <> BSC.pack (show iw1) <> "-" <> BSC.pack (show iw2)
 	elementIdGen _ = error "not yet implemented"
 
 diagramM :: CBState -> Maybe Pos -> [OWire] -> DiagramMapM BasicGate
 diagramM cbs mpos [o] = case cbsGate cbs  !? o of
-	Just e@(NotGate iw) -> do
-		ip <- inputPosition =<< lift . maybe (Left "Oops") Right =<< case mpos of
-			Nothing -> putElement0 (BG e) notGateD
-			Just ps -> putElement (BG e) notGateD ps
-		case cbsWireConn cbs !? iw of
-			Just [(o', _)] -> do
-				bg <- diagramM cbs (Just ip) [o']
-				connectLine (BG e) (BG bg)
-				return ()
-			Nothing -> return ()
-			_ -> lift $ Left "not yet implemented"
+	Just e -> do
+		(ips, iws) <- case e of 
+			NotGate iw' -> do
+				ip' <- inputPosition =<< lift . maybe (Left "Oops") Right =<< case mpos of
+					Nothing -> putElement0 (BG e) notGateD
+					Just ps -> putElement (BG e) notGateD ps
+				return ([ip'], [iw'])
+			AndGate iw1 iw2 -> do
+				(ip1, ip2) <- (\(m1, m2) -> (,) <$> m1 <*> m2) . (inputPosition1 &&& inputPosition2)
+					=<< lift . maybe (Left "Oops2") Right =<< case mpos of
+						Nothing -> putElement0 (BG e) andGateD
+						Just ps -> putElement (BG e) andGateD ps
+				return ([ip1, ip2], [iw1, iw2])
+			_ -> lift $ Left "Oops!"
+		nextDiagramM cbs e ips iws
 		return e
 	_ -> lift $ Left "not yet implemented"
 diagramM _ _ _ = lift $ Left "not yet implemented"
 
-sampleNotGate :: (OWire, CBState)
-sampleNotGate = (`runState` initCBState) $ do
-	(_, ow) <- notGate
-	(iw', ow') <- notGate
-	(iw'', ow'') <- notGate
-	connectWire64 ow iw'
-	connectWire64 ow' iw''
-	return ow''
+nextDiagramM :: CBState -> BasicGate -> [Pos] -> [IWire] -> DiagramMapM ()
+nextDiagramM cbs e [ip] [iw] = do
+	case cbsWireConn cbs !? iw of
+		Just [(o', _)] -> do
+			bg <- diagramM cbs (Just ip) [o']
+			connectLine (BG e) (BG bg)
+			return ()
+		Nothing -> return ()
+		_ -> lift $ Left "not yet implemented"
+nextDiagramM cbs e [ip1, ip2] [iw1, iw2] = do
+	case cbsWireConn cbs !? iw1 of
+		Just [(o', _)] -> do
+			bg <- diagramM cbs (Just ip1) [o']
+			connectLine1 (BG e) (BG bg)
+		Nothing -> return ()
+		_ -> lift $ Left "not yet implemented"
+	case cbsWireConn cbs !? iw2 of
+		Just [(o', _)] -> do
+			bg <- diagramM cbs (Just ip2) [o']
+			connectLine2 (BG e) (BG bg)
+		Nothing -> return ()
+		_ -> lift $ Left "not yet implemented"
+nextDiagramM _ _ _ _ = lift $ Left "Oops!!!!!!!!!!!!!!!!!!"
---
+++ src/TryDiagram.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs -fno-warn-unused-imports #-}
+
+module TryDiagram where
+
+import Control.Monad.State
+import Diagrams.Prelude
+import Diagrams.Backend.SVG
+
+import Circuit.Adornt.Builder
+import Circuit.DiagramDsl
+
+import Circuit.Adornt.Diagram
+
+sampleNotGate :: (OWire, CBState)
+sampleNotGate = (`runState` initCBState) $ do
+	(_, ow) <- notGate
+	(iw', ow') <- notGate
+	(iw'', ow'') <- notGate
+	connectWire64 ow iw'
+	connectWire64 ow' iw''
+	return ow''
+
+sampleAndGate :: (OWire, CBState)
+sampleAndGate = (`runState` initCBState) $ do
+	(_, _, ow) <- andGate
+	(_, _, ow') <- andGate
+	(iw1'', iw2'', ow'') <- andGate
+	connectWire64 ow iw1''
+	connectWire64 ow' iw2''
+	return ow''
+
+sampleNandGate :: (OWire, CBState)
+sampleNandGate = (`runState` initCBState) $ do
+	(_, _, ow) <- andGate
+	(ni, no) <- notGate
+	connectWire64 ow ni
+	return no
