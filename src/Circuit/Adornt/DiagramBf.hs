3dc9ac4d210786b76b51129b9598401336a8ccb0
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-08-18 12:57:14 +0900

    prepare for new interface of circuit-diagram-dsl

--- src/Circuit/Adornt/DiagramBf.hs
+++ src/Circuit/Adornt/DiagramBf.hs
@@ -5,14 +5,27 @@ module Circuit.Adornt.DiagramBf where
 
 import Data.Map.Strict
 import Circuit.Adornt.Builder
-import Circuit.DiagramDslOld
+import Circuit.DiagramDsl
 
 import qualified Data.ByteString.Char8 as BSC
+import qualified Circuit.DiagramDslOld as O
 
 newtype ElemId = EidOWire OWire deriving Show
 
 instance ElementIdable ElemId where
 	elementId (EidOWire o) = "OWire-" <> BSC.pack (show o)
+
+type Connection = ElemId -> DiagramMapM ()
+
+diagramBfM2 :: CBState -> [(Maybe (Connection, Pos), OWire)] -> DiagramMapM ()
+diagramBfM2 _ [] = return ()
+diagramBfM2 cbs ((mpre, o) : os) = do
+	case cbsGate cbs !? o of
+		Just e -> case e of
+			NotGate iw -> case mpre of
+				Nothing -> () <$ putElementEnd1 eid notGateD
+	where
+	eid = EidOWire o
 
 diagramBfM :: CBState -> [(Maybe (ElemId, Int, Pos), OWire)] -> DiagramMapM ()
 diagramBfM _ [] = return ()
@@ -22,25 +35,25 @@ diagramBfM cbs ((mpre, o) : os) = do
 			miwps <- case e of
 				NotGate iw -> do
 					mlps <- case mpre of
-						Nothing -> putElement0 eid notGateD
-						Just (preid, _, ips) -> putElement eid notGateD ips
+						Nothing -> O.putElement0 eid O.notGateD
+						Just (preid, _, ips) -> O.putElement eid O.notGateD ips
 					mps <- case mlps of
-						Just lps -> Just <$> inputPosition lps
+						Just lps -> Just <$> O.inputPosition lps
 						Nothing -> return Nothing
 					return $ (: []) . (iw ,) <$> mps
 				OrGate iw1 iw2 -> do
 					mlps <- case mpre of
-						Nothing -> putElement0 eid orGateD
-						Just (preid, _, ips) -> putElement eid orGateD ips
+						Nothing -> O.putElement0 eid O.orGateD
+						Just (preid, _, ips) -> O.putElement eid O.orGateD ips
 					mps <- case mlps of
 						Just lps -> (Just <$>)
-							$ (\a b -> [a, b]) <$> inputPosition1 lps <*> inputPosition2 lps
+							$ (\a b -> [a, b]) <$> O.inputPosition1 lps <*> O.inputPosition2 lps
 						Nothing -> return Nothing
 					return $ zipWith (,) [iw1, iw2] <$> mps
 			case mpre of
-				Just (p, 0, _) -> connectLine p eid
-				Just (p, 1, _) -> connectLine1 p eid
-				Just (p, 2, _) -> connectLine2 p eid
+				Just (p, 0, _) -> O.connectLine p eid
+				Just (p, 1, _) -> O.connectLine1 p eid
+				Just (p, 2, _) -> O.connectLine2 p eid
 				Nothing -> return ()
 			case miwps of
 				Just [(iw, ps)] -> do
